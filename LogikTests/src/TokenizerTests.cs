b966b242747ec90fec5b0f1c7b84376e1d76e756
Sebastian Uribe <suribe@gmail.com>
2021-04-03 22:31:05 +0200

    WIP: Implement Shunting Yard algorithm for parsing expressions into postfix notation

--- LogikTests/src/TokenizerTests.cs
+++ LogikTests/src/TokenizerTests.cs
@@ -3,60 +3,80 @@ using NUnit.Framework;
 
 namespace Logik.Tests.Core {
 
-    internal class TokenTest {
+    public class ParsingTest {
+        
+        public const string NumericValueOne = "1";
+
+        public const string OnePlusTwo = "1 + 2";
+        public const string OnePlusTwoNoSpaces = "1+2";
+        public const string OnePlusTwoSpaceBeforeOp = "1 +2";
+        public const string OnePlusTwoSpaceAfterOp = "1+ 2";
+        public const string OnePlusTwoUntrimmed = " 1 + 2 ";
+        public const string OnePlusTwoManySpaces = "  1    +   2    ";
+
+        public static string[] OnePlusTwoExpected = {"1", "+", "2" };
+        public static string[] OnePlusTwoPostfix = {"1", "2", "+" };
+
+        public const string MultipleOperations = "1 + 2 * 3 / 4 - 5";
+        public static string[] MultipleOperationsExpected = {"1", "+", "2", "*", "3", "/", "4", "-", "5" };
+
+        public const string SimpleParens = "(1 + 2) * 3";
+        public static string[] SimpleParensExpected = {"(", "1", "+", "2", ")", "*", "3"};
+
+        public const string NestedParens = "((1 + 2) * 3)";
+        public static string[] NestedParensExpected = {"(", "(", "1", "+", "2", ")", "*", "3",")"};
+
+        public const string LotsOfParens = "( ((1 + (2)) ) * 3)";
+        public static string[] LotsOfParensExpected = {"(", "(", "(", "1", "+", "(", "2", ")",")",")", "*", "3",")"};
+
+        public const string UnaryMinus = "-(1 + -2) * 3";
+        public static string[] UnaryMinusExpected = {"-","(","1","+","-","2",")","*","3"};
+
+        public TokenTestCase[] testsWithSpaces = {
+            new TokenTestCase(OnePlusTwo, OnePlusTwoExpected),
+            new TokenTestCase(OnePlusTwoNoSpaces, OnePlusTwoExpected),
+            new TokenTestCase(OnePlusTwoSpaceBeforeOp, OnePlusTwoExpected),
+            new TokenTestCase(OnePlusTwoSpaceAfterOp, OnePlusTwoExpected),
+            new TokenTestCase(OnePlusTwoUntrimmed, OnePlusTwoExpected),
+            new TokenTestCase(OnePlusTwoManySpaces, OnePlusTwoExpected),
+        };
+
+        public TokenTestCase[] testsWithParens = {
+            new TokenTestCase(SimpleParens, SimpleParensExpected),
+            new TokenTestCase(NestedParens, NestedParensExpected),
+            new TokenTestCase(LotsOfParens, LotsOfParensExpected),
+        };
+    }
+
+    public class TestParser : ParsingTest {
+        private FormulaParser parser;
+
+        [SetUp]
+        public void Setup() {
+            parser = new FormulaParser();
+        }
+
+        [Test]
+        public void TestSimpleParser() {
+            var tokenizer = new Tokenizer(OnePlusTwo);
+            var postfix = parser.ToPostfix(tokenizer.Tokens);
+            CollectionAssert.AreEqual(OnePlusTwoPostfix, postfix);
+        }
+    }
+
+    public class TokenTestCase {
         public readonly string input;
         public readonly string[] expected;
 
-        public TokenTest(string input, string[] expected) {
+        public TokenTestCase(string input, string[] expected) {
             this.input = input;
             this.expected = expected;
         }
     }
 
-    public class TestTokenizer {
+    public class TestTokenizer : ParsingTest {
         private Tokenizer tokenizer;
         private string input;
-
-        private const string NumericValueOne = "1";
-
-        private const string OnePlusTwo = "1 + 2";
-        private const string OnePlusTwoNoSpaces = "1+2";
-        private const string OnePlusTwoSpaceBeforeOp = "1 +2";
-        private const string OnePlusTwoSpaceAfterOp = "1+ 2";
-        private const string OnePlusTwoUntrimmed = " 1 + 2 ";
-        private const string OnePlusTwoManySpaces = "  1    +   2    ";
-
-        private static string[] OnePlusTwoExpected = {"1", "+", "2" };
-
-        private const string MultipleOperations = "1 + 2 * 3 / 4 - 5";
-        private static string[] MultipleOperationsExpected = {"1", "+", "2", "*", "3", "/", "4", "-", "5" };
-
-        private const string SimpleParens = "(1 + 2) * 3";
-        private static string[] SimpleParensExpected = {"(", "1", "+", "2", ")", "*", "3"};
-
-        private const string NestedParens = "((1 + 2) * 3)";
-        private static string[] NestedParensExpected = {"(", "(", "1", "+", "2", ")", "*", "3",")"};
-
-        private const string LotsOfParens = "( ((1 + (2)) ) * 3)";
-        private static string[] LotsOfParensExpected = {"(", "(", "(", "1", "+", "(", "2", ")",")",")", "*", "3",")"};
-
-        private const string UnaryMinus = "-(1 + -2) * 3";
-        private static string[] UnaryMinusExpected = {"-","(","1","+","-","2",")","*","3"};
-
-        private TokenTest[] testsWithSpaces = {
-            new TokenTest(OnePlusTwo, OnePlusTwoExpected),
-            new TokenTest(OnePlusTwoNoSpaces, OnePlusTwoExpected),
-            new TokenTest(OnePlusTwoSpaceBeforeOp, OnePlusTwoExpected),
-            new TokenTest(OnePlusTwoSpaceAfterOp, OnePlusTwoExpected),
-            new TokenTest(OnePlusTwoUntrimmed, OnePlusTwoExpected),
-            new TokenTest(OnePlusTwoManySpaces, OnePlusTwoExpected),
-        };
-
-        private TokenTest[] testsWithParens = {
-            new TokenTest(SimpleParens, SimpleParensExpected),
-            new TokenTest(NestedParens, NestedParensExpected),
-            new TokenTest(LotsOfParens, LotsOfParensExpected),
-        };
 
         [SetUp]
         public void Setup() { }
@@ -104,7 +124,7 @@ namespace Logik.Tests.Core {
             ThenTokensAre(expected);
         }
 
-        private void RunMultipleTests(TokenTest[] tests) {
+        private void RunMultipleTests(TokenTestCase[] tests) {
             foreach (var test in tests) {
                 WhenInputIs(test.input);
                 Tokenize();
--- src/logik/core/Tokenizer.cs
+++ src/logik/core/Tokenizer.cs
@@ -3,6 +3,52 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Logik.Core {
+
+    // http://www.wcipeg.com/wiki/Shunting_yard_algorithm
+    // http://math.oxford.emory.edu/site/cs171/shuntingYardAlgorithm/
+
+    public class FormulaParser {
+
+        private static string Operators = "+-/*";
+        private static int[] Precedence = {2, 2, 3, 3};
+
+        private bool HigherPrecedence(string op1, string op2) {
+            return Precedence[Operators.IndexOf(op1)] > Precedence[Operators.IndexOf(op2)];
+        }
+
+        private bool IsOperator(string token) {
+            return Operators.Contains(token);
+        }
+
+        public List<string> ToPostfix(List<string> tokens) {
+            Stack<string> opstack = new Stack<string>();
+            Queue<string> postfix = new Queue<string>();
+
+            foreach (var token in tokens) {
+                if (IsOperator(token)) {
+                    if (opstack.Count == 0) {
+                        opstack.Push(token);
+                    } else {
+                        var prev = opstack.Peek();
+                        if (HigherPrecedence(prev, token)) {
+                            opstack.Pop();
+                            postfix.Enqueue(prev);
+                        }
+                        opstack.Push(token);
+                    }
+                } else {
+                    postfix.Enqueue(token);
+                }
+            }
+            while (opstack.Count > 0) {
+                postfix.Enqueue(opstack.Pop());
+            }
+
+            return new List<string>(postfix);   
+        }
+    }
+
+
     public class Tokenizer {
         public List<string> Tokens { private set; get; }
 

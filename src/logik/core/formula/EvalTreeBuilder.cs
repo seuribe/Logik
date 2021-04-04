ee6b30c4e1ad0a8f7e7c1946df340143b71072b1
Sebastian Uribe <suribe@gmail.com>
2021-04-04 23:03:06 +0200

    Add Tree based evaluator

--- src/logik/core/formula/EvalTreeBuilder.cs
+++ src/logik/core/formula/EvalTreeBuilder.cs
@@ -1,11 +1,56 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Logik.Core.Formula {
     
     public delegate float OpFunction(List<EvalNode> children);
 
+    public class TreeEvaluator : IEvaluator {
+        private Dictionary<string, Cell> cells = new Dictionary<string, Cell>();
+        private Dictionary<string, EvalNode> trees = new Dictionary<string, EvalNode>();
+        private readonly EvalTreeBuilder builder;
+
+        public TreeEvaluator() {
+            builder = new EvalTreeBuilder(Lookup);
+        }
+
+        private float Lookup(string id) {
+            return float.Parse(cells[id].Value);
+        }
+
+        public void Define(Cell cell) {
+            cells[cell.Id] = cell;
+            var tokens = Tokenizer.ProcessInput(cell.Formula);
+            var parser = new FormulaParser(tokens);
+            var tree = builder.BuildTree(parser.Output);
+            trees[cell.Id] = tree;
+        }
+
+        public string Evaluate(Cell cell) {
+            return trees[cell.Id].Eval().ToString();
+        }
+
+        public List<string> References(Cell cell) {
+            var referenceNodes = trees[cell.Id].Collect( node => node is ExternalReferenceNode);
+            return referenceNodes.Select( node => (node as ExternalReferenceNode).Name).ToList();
+        }
+
+        public void Undefine(Cell cell) {
+            cells.Remove(cell.Id);
+            trees.Remove(cell.Id);
+        }
+    }
+
+    public delegate bool NodePredicate(EvalNode node);
+
     public abstract class EvalNode {
         public abstract float Eval();
+        public virtual IEnumerable<EvalNode> Collect(NodePredicate predicate) {
+            if (predicate(this))
+                return new List<EvalNode>() { this };
+            else
+                return Enumerable.Empty<EvalNode>();
+        }
     }
 
     public class ValueNode : EvalNode {
@@ -26,16 +71,16 @@ namespace Logik.Core.Formula {
     public delegate float ValueLookup(string name);
 
     public class ExternalReferenceNode : EvalNode {
-        private readonly string name;
+        public readonly string Name;
         private readonly ValueLookup lookupFunction;
 
         public ExternalReferenceNode(string name, ValueLookup lookupFunction) {
-            this.name = name;
+            this.Name = name;
             this.lookupFunction = lookupFunction;
         }
 
         public override float Eval() {
-            return lookupFunction(name);
+            return lookupFunction(Name);
         }
     }
 
@@ -63,6 +108,17 @@ namespace Logik.Core.Formula {
                 return "(" + children[0] + " " + OpToken + " " + children[1] + ")";
             else
                 return "(" + OpToken + string.Join(" ", children.ConvertAll( c => c.Eval().ToString()).ToArray()) + ")";
+        }
+
+        public override IEnumerable<EvalNode> Collect(NodePredicate predicate) {
+            List<EvalNode> ret = new List<EvalNode>();
+            if (predicate(this))
+                ret.Add(this);
+
+            foreach (var child in children)
+                ret.AddRange(child.Collect(predicate));
+
+            return ret;
         }
     }
 

f4092c22f88117680b3ad657273f06fb6bc21cdc
Sebastian Uribe <suribe@gmail.com>
2021-04-04 11:35:09 +0200

    Move Scheme dependant tests to own file

---
+++ LogikTests/src/TestUScheme.cs
@@ -0,0 +1,16 @@
+﻿using Logik.Core;
+using NUnit.Framework;
+using System;
+
+namespace Logik.Tests.Core {
+    public class TestUScheme : CellTestBase {
+
+        [Test]
+        public void EvalBrokenExpressions() {
+            WhenFormulaIs(cell, "1");
+            ThenCellHasNoError(cell2);
+            WhenFormulaIs(cell2, $"{cell.Id})");
+            ThenCellHasError(cell2);
+        }
+    }
+}

8831d0715e051e3adb5ad7becfeb139a94f8acdf
Sebastian Uribe <suribe@gmail.com>
2021-04-04 22:15:21 +0200

    Rename Evaluator class

--- src/logik/core/SchemeEvaluator.cs
+++ src/logik/core/SchemeEvaluator.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UScheme;
 
 namespace Logik.Core {
-    public class Evaluator {
+    public class SchemeEvaluator {
         Env env = new Env(Env.Global);
+        Env formulas = new Env(Env.Global);
 
-        public void DefineCell(Cell cell, string formula) {
-            var defString = $"(define ({cell.Id}) {formula})";
-            var defExpression = UScheme.Parser.Parse(defString);
+        public void DefineCell(Cell cell) {
+            var defString = $"(define {cell.Id} {cell.Value})";
+            var defExpression = Parser.Parse(defString);
             UScheme.UScheme.Eval(defExpression, env);
+
+            defString = $"(define ({cell.Id}) {cell.Formula})";
+            defExpression = Parser.Parse(defString);
+            UScheme.UScheme.Eval(defExpression, formulas);
+        }
+
+        public void UndefineCell(Cell cell) {
+            env.Remove(cell.Id);
+            formulas.Remove(cell.Id);
         }
 
         public string EvaluateCell(Cell cell) {
-            var callExpression = UScheme.Parser.Parse($"({cell.Id})");
-            var ret = UScheme.UScheme.Eval(callExpression, env);
+            var formulaExp = formulas.Get(cell.Id);
+            var ret = UScheme.UScheme.Eval(formulaExp, env);
             return ret.ToString();
         }
 

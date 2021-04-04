bc87315836540a29de960099f725202908316e06
Sebastian Uribe <suribe@gmail.com>
2021-04-04 23:01:14 +0200

    Add Evaluator interface

---
+++ src/logik/core/IEvaluator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Logik.Core {
+    public interface IEvaluator {
+        void Define(Cell cell);
+        void Undefine(Cell cell);
+        string Evaluate(Cell cell);
+        List<string> References(Cell cell);
+    }
+}

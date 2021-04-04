215692e1a5f9287e4632f8202c8755d7a3b7dc32
Sebastian Uribe <suribe@gmail.com>
2021-04-04 11:32:58 +0200

    Add Formula Parsing Test File

---
+++ LogikTests/src/TestParser.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Logik.Tests.Core {
+
+    public class TestParser : ParsingTest {
+        [Test]
+        public void PostfixSimpleSum() {
+            WhenTokenizing(OnePlusTwo);
+            WhenParsingTokens();
+            ThenPostfixIs(OnePlusTwoPostfix);
+        }
+
+        [Test]
+        public void PostfixOperatorPrecedence() {
+            WhenTokenizing(OnePlusTwoTimesThree);
+            WhenParsingTokens();
+            ThenPostfixIs(OnePlusTwoTimesThreePostFix);
+
+            WhenTokenizing(OneTimesTwoPlusThree);
+            WhenParsingTokens();
+            ThenPostfixIs(OneTimesTwoPlusThreePostFix);
+        }
+    }
+}

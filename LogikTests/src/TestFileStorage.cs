15cb707a5b1346cb220306fcb443357f7b373f4c
Sebastian Uribe <suribe@gmail.com>
2021-04-08 11:27:25 +0200

    Add storage tests

--- LogikTests/src/TestFileStorage.cs
+++ LogikTests/src/TestFileStorage.cs
@@ -2,8 +2,9 @@
 using Logik.Core.Formula;
 using Logik.Storage;
 using NUnit.Framework;
+using System.IO;
 
-namespace Logik.Tests {
+namespace Logik.Tests.Storage {
 
     public class TestStorage : CellTestBase {
         ModelStorage storage;
@@ -31,6 +32,27 @@ namespace Logik.Tests {
             ThenFormulaIs(cell2, "65");
             ThenFormulaIs(cell3, "C1 * 5");
             ThenFormulaIs(cell4, "C2 + C3 + C1");
+        }
+
+        [Test]
+        public void CellRenamingIsPreserved() {
+            CanChangeName(cell, "celda");
+            WhenModelIsSavedIn("rename_test.logik");
+            WhenModelIsReset();
+            WhenModelIsRestoredFrom("rename_test.logik");
+            WhenCellsAreRestoredFromModel();
+            ThenNameIs(cell, "celda");
+        }
+
+        [Test]
+        public void CannotReadUnknownEvaluator() {
+            string strModel = @"{""evaluator"":""udfsakfuashdlaksidshaladjl"",""cells"":[{""name"":""C1"",""formula"":""10""}]}";
+            var bytes = System.Text.Encoding.UTF8.GetBytes(strModel);
+            MemoryStream ms = new MemoryStream(bytes);
+            using (JsonModelReader reader = new JsonModelReader(ms)) {
+                TestDelegate evalCall = () => reader.ReadModel();
+                Assert.Throws(Is.InstanceOf<System.Exception>(), evalCall);
+            }
         }
 
         private void WhenModelIsRestoredFrom(string filename) {

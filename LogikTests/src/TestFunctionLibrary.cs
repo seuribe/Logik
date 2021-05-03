8c04d5a451bdcd48623637b233a90721349be819
Sebastian Uribe <suribe@gmail.com>
2021-05-03 15:40:54 +0200

    Move Function Library tests to own class

---
+++ LogikTests/src/TestFunctionLibrary.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Logik.Tests.Core {
+    public class TestFunctionLibrary : ParsingTest {
+                
+        [Test]
+        public void Average() {
+            WhenBuildingTree("average(1; 2; 10; 6; 3)");
+            ThenTreeEvalsTo(4.4f);
+        }
+                
+        [Test]
+        public void Min() {
+            WhenBuildingTree("min(1; 2; 10; 6; 3)");
+            ThenTreeEvalsTo(1);
+        }
+                
+        [Test]
+        public void Max() {
+            WhenBuildingTree("max(1; 2; 10; 6; 3)");
+            ThenTreeEvalsTo(10);
+        }
+    }
+}
--- LogikTests/src/TestTreeBuilder.cs
+++ LogikTests/src/TestTreeBuilder.cs
@@ -4,27 +4,6 @@ using System.Collections.Generic;
 
 namespace Logik.Tests.Core {
 
-    public class TestFunctionLibrary : ParsingTest {
-                
-        [Test]
-        public void Average() {
-            WhenBuildingTree("average(1; 2; 10; 6; 3)");
-            ThenTreeEvalsTo(4.4f);
-        }
-                
-        [Test]
-        public void Min() {
-            WhenBuildingTree("min(1; 2; 10; 6; 3)");
-            ThenTreeEvalsTo(1);
-        }
-                
-        [Test]
-        public void Max() {
-            WhenBuildingTree("max(1; 2; 10; 6; 3)");
-            ThenTreeEvalsTo(10);
-        }
-    }
-
     public class TestTreeBuilder : ParsingTest {
         [Test]
         public void EvalSimpleSum() {

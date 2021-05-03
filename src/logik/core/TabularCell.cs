d064dec8be14b505e718b54a8a0d84dc49ccbb24
Sebastian Uribe <suribe@gmail.com>
2021-05-03 15:40:06 +0200

    Small code cleanup/refactor

--- src/logik/core/TabularCell.cs
+++ src/logik/core/TabularCell.cs
@@ -4,27 +4,31 @@ namespace Logik.Core {
 
     public class TabularCell : ICell {
         public string Name { get; private set; }
-        public bool Error { get; private set; }
+        public bool Error { get; private set; } = false;
         public string ErrorMessage { get; private set; }
 
-        private float[,] data = {{ 0 }};
         public int Rows { get; private set; } = 1;
         public int Columns { get; private set; } = 1;
 
         public event CellEvent ErrorStateChanged;
 
+        private float[,] data = {{ 0 }};
+
+
         public float this[int row, int column] {
             get {
-                if (row < 0 || row >= Rows || column < 0 || column >= Columns)
-                    throw new LogikException($"Invalid Index. {row},{column} is outside {Name}'s size of {Rows}x{Columns}");
-
+                CheckRange(row, column);
                 return data[row, column];
             }
             set {
-                if (row < 0 || row >= Rows || column < 0 || column >= Columns)
-                    throw new LogikException($"Invalid Index. {row},{column} is outside {Name}'s size of {Rows}x{Columns}");
+                CheckRange(row, column);
                 data[row, column] = value;
             }
+        }
+
+        private void CheckRange(int row, int column) {
+            if (row < 0 || row >= Rows || column < 0 || column >= Columns)
+                throw new LogikException($"Invalid Index. {row},{column} is outside {Name}'s size of {Rows}x{Columns}");
         }
 
         public void Resize(int rows, int columns) {
@@ -38,9 +42,7 @@ namespace Logik.Core {
             if (rows <= data.GetLength(0) && columns <= data.GetLength(1))
                 return;
 
-            var newData = new float[
-                Math.Max(rows, Rows),
-                Math.Max(columns, Columns)];
+            var newData = new float[ Math.Max(rows, Rows), Math.Max(columns, Columns) ];
 
             for (int r = 0 ; r < Rows ; r++)
                 for (int c = 0 ; c < Columns; c++)

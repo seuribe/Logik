ce050a60aec7c523148829bbcb80683db55eee85
Sebastian Uribe <suribe@gmail.com>
2021-05-04 15:28:18 +0200

    Grid is centered in TabularCell

--- src/TableCellView.cs
+++ src/TableCellView.cs
@@ -6,12 +6,16 @@ public class TableCellView : Control {
 
 	private TabularCell tcell;
 	private Control valueGrid;
+	private Panel mainPanel;
 	private LineEdit viewTemplate;
 	private LineEdit[,] valueViews = new LineEdit[1,1];
 	private Rect2 viewRect;
+	private readonly float HorizontalMargin = 16;
+	private readonly float VerticalMargin = 16;
 
 	public override void _Ready() {
-		valueGrid = GetNode<Control>("Main/Grid");
+		mainPanel = GetNode<Panel>("Main");
+		valueGrid = mainPanel.GetNode<Control>("Grid");
 		viewTemplate = GetNode<LineEdit>("ValueTemplate");
 		viewRect = viewTemplate.GetRect();
 
@@ -31,6 +35,9 @@ public class TableCellView : Control {
 
 	private void CreateGrid() {
 		valueViews = new LineEdit[tcell.Rows, tcell.Columns];
+		mainPanel.RectSize = new Vector2(
+			tcell.Columns * viewRect.Size.x + HorizontalMargin,
+			tcell.Rows * viewRect.Size.y + VerticalMargin);
 		for (int row = 0 ; row < tcell.Rows ; row++) {
 			for (int column = 0 ; column < tcell.Columns ; column++) {
 				var view = viewTemplate.Duplicate() as LineEdit;
@@ -43,7 +50,8 @@ public class TableCellView : Control {
 	}
 
 	private Vector2 GetCellPosition(int row, int column) => 
-		new Vector2(viewRect.Position.x + column * viewRect.Size.x, row * viewRect.Size.y);
+		new Vector2(HorizontalMargin/2 + column * viewRect.Size.x,
+					VerticalMargin/2 + row * viewRect.Size.y);
 
 
 	private void ClearGrid() {

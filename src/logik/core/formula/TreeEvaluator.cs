91ef6541c29c82a48cc2c9b530e83e781336d053
Sebastian Uribe <suribe@gmail.com>
2021-05-04 18:48:44 +0200

    refactor: further remove code from TreeEvaluator

--- src/logik/core/formula/TreeEvaluator.cs
+++ src/logik/core/formula/TreeEvaluator.cs
@@ -15,26 +15,16 @@ namespace Logik.Core.Formula {
 
         public static string EvaluatorType = "default";
 
-        private Dictionary<string, NumericCell> cells = new Dictionary<string, NumericCell>();
-        private Dictionary<string, TabularCell> tcells = new Dictionary<string, TabularCell>();
-
         public string Type => EvaluatorType;
 
-        private float Lookup(string id) => cells[id].Value;
-
-        private float TabularLookup(string id, int row, int column) => tcells[id][row, column];
-
         public void Define(TabularCell tcell) {
-            tcells[tcell.Name] = tcell;
         }
 
         public void Define(NumericCell cell) {
-            cells[cell.Name] = cell;
-            cell.EvalNode = new EvalNodeBuilder().Build(cell.Formula, Lookup, TabularLookup);
         }
 
         public float Evaluate(NumericCell cell) {
-            return cell.EvalNode.Eval();
+            return 0;
         }
 
         public List<string> References(NumericCell cell) {
@@ -43,12 +33,9 @@ namespace Logik.Core.Formula {
         }
 
         public void Undefine(NumericCell cell) {
-            cells.Remove(cell.Name);
         }
 
         public void Rename(NumericCell cell, string newName) {
-            cells[newName] = cells[cell.Name];
-            cells.Remove(cell.Name);
         }
     }
 }

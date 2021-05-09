05a3bcdc84e8dc4a8195bf0a0f36a533234fd192
Sebastian Uribe <suribe@gmail.com>
2021-05-09 13:20:41 +0200

    Select/Deselect all when Grid Cell receives/loses focus

--- src/GridCell.cs
+++ src/GridCell.cs
@@ -10,7 +10,17 @@ public class GridCell : NameEdit {
 	public override void _Ready() {
 		Connect("text_entered", this, nameof(OnTextEntered));
 		Connect("text_changed", this, nameof(OnTextEntered));
+		Connect("focus_entered", this, nameof(OnFocusEntered));
+		Connect("focus_exited", this, nameof(OnFocusExited));
 	}
+
+	private void OnFocusExited() {
+		Deselect();
+    }
+
+	private void OnFocusEntered() {
+		SelectAll();
+    }
 
 	private void OnTextEntered(string newText) {
 		ContentChanged?.Invoke(Row, Column);

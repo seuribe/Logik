d69d06fb31815afd2c8c74cb454a4b99771c754e
Sebastian Uribe <suribe@gmail.com>
2021-05-13 17:19:49 +0200

    Formula and Builder

---
+++ src/logik/core/formula/Formula.cs
@@ -0,0 +1,23 @@
+﻿namespace Logik.Core.Formula {
+    public class Formula {
+
+        public readonly string Text;
+
+        private readonly EvalNode evalNode;
+
+        public Formula() {
+            Text = "0";
+            evalNode = new ValueNode(0);
+        }
+
+        public Formula(string formula, ValueLookup valueLookup, TabularLookup tabularLookup) {
+            Text = formula;
+            var tokens = new Tokenizer(formula).Tokens;
+            var postfix = new FormulaParser(tokens).Output;
+            evalNode = new EvalTreeBuilder(postfix, valueLookup, tabularLookup).Root;
+        }
+
+        public Value Eval() => evalNode.Eval();
+
+    }
+}
---
+++ src/logik/core/formula/FormulaBuilder.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Logik.Core.Formula {
+
+    public class FormulaBuilder {
+        private readonly ValueLookup valueLookup;
+        private readonly TabularLookup tabularLookup;
+
+        public FormulaBuilder(ValueLookup valueLookup, TabularLookup tabularLookup) {
+            this.valueLookup = valueLookup;
+            this.tabularLookup = tabularLookup;
+        }
+
+        public Formula Build(string formula) {
+            return new Formula(formula, valueLookup, tabularLookup);
+        }
+    }
+}

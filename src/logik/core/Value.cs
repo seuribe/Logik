8dbb16f18d542fcd7fb7e7a34e0a37909dd04240
Sebastian Uribe <suribe@gmail.com>
2021-05-15 17:49:24 +0200

    Support Bool values in Value

--- src/logik/core/Value.cs
+++ src/logik/core/Value.cs
@@ -9,14 +9,17 @@
         public float AsFloat { get => (data == null) ? 0f : float.Parse(data); }
         public int AsInt { get  => (data == null) ? 0 : int.Parse(data); }
         public string AsString { get => (data == null) ? "" : data; }
+        public bool AsBool { get => (data != null) && (data != "") && (data != "0"); }
 
         public static implicit operator Value(float value) => new Value(value.ToString());
         public static implicit operator Value(int value) => new Value(value.ToString());
         public static implicit operator Value(string value) => new Value(value);
+        public static implicit operator Value(bool value) => new Value(value ? "1" : "0");
 
         public static implicit operator float(Value value) => value.AsFloat;
         public static implicit operator int(Value value) => value.AsInt;
         public static implicit operator string(Value value) => value.data;
+        public static implicit operator bool(Value value) => value.AsBool;
 
         public override string ToString() {
             return AsString;

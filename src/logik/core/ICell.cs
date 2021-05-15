0f9d696e6eb48a0082f1fbe3f911f3984b37d51e
Sebastian Uribe <suribe@gmail.com>
2021-05-15 17:45:42 +0200

    Move Value to own file

--- src/logik/core/ICell.cs
+++ src/logik/core/ICell.cs
@@ -6,30 +6,6 @@ namespace Logik.Core {
     public delegate void CellEvent(ICell cell);
     public delegate void CellNameEvent(ICell cell, string name);
 
-    public struct Value {
-        private readonly string data;
-
-        private Value(string data) {
-            this.data = data;
-        }
-
-        public float AsFloat { get => (data == null) ? 0f : float.Parse(data); }
-        public int AsInt { get  => (data == null) ? 0 : int.Parse(data); }
-        public string AsString { get => (data == null) ? "" : data; }
-
-        public static implicit operator Value(float value) => new Value(value.ToString());
-        public static implicit operator Value(int value) => new Value(value.ToString());
-        public static implicit operator Value(string value) => new Value(value);
-
-        public static implicit operator float(Value value) => value.AsFloat;
-        public static implicit operator int(Value value) => value.AsInt;
-        public static implicit operator string(Value value) => value.data;
-
-        public override string ToString() {
-            return AsString;
-        }
-    }
-
     /// <summary>
     /// Interface for all cells in a model.
     /// Core functionalities are name, error state and references, and the listeners required
---
+++ src/logik/core/Value.cs
@@ -0,0 +1,25 @@
+﻿namespace Logik.Core {
+    public struct Value {
+        private readonly string data;
+
+        private Value(string data) {
+            this.data = data;
+        }
+
+        public float AsFloat { get => (data == null) ? 0f : float.Parse(data); }
+        public int AsInt { get  => (data == null) ? 0 : int.Parse(data); }
+        public string AsString { get => (data == null) ? "" : data; }
+
+        public static implicit operator Value(float value) => new Value(value.ToString());
+        public static implicit operator Value(int value) => new Value(value.ToString());
+        public static implicit operator Value(string value) => new Value(value);
+
+        public static implicit operator float(Value value) => value.AsFloat;
+        public static implicit operator int(Value value) => value.AsInt;
+        public static implicit operator string(Value value) => value.data;
+
+        public override string ToString() {
+            return AsString;
+        }
+    }
+}
